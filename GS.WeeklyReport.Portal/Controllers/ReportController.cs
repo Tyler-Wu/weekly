9e7e736968f916415760a87071fbfc8650ed280b
tonywang <wangyun122@gmail.com>
2014-05-19 00:14:47 +0800

    准备开始完成report页面。

--- GS.WeeklyReport.Portal/Controllers/ReportController.cs
+++ GS.WeeklyReport.Portal/Controllers/ReportController.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GS.WeeklyReport.IService;
+using GS.WeeklyReport.Service;
 
 namespace GS.WeeklyReport.Portal.Controllers
 {
     public class ReportController : BaseController
     {
+        IProjectService projectService=new ProjectService();
         //
         // GET: /Report/
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult InitChartInfo()
+        {
+            var categories = projectService.LoadEntities(p => true).Select(p=>new {p.Name,p.ProjectId});
+            return Json("11");
         }
 	}
 }
