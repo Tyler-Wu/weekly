493e1c9ec9091a0390b49b3a9750f5272d8e50d5
Jason hao <Jason.hao@contractor.bpiedu.com>
2014-07-05 13:15:35 +0800

    project view

--- GS.WeeklyReport.Portal/Controllers/ProjectController.cs
+++ GS.WeeklyReport.Portal/Controllers/ProjectController.cs
@@ -11,12 +11,28 @@ namespace GS.WeeklyReport.Portal.Controllers
     public class ProjectController : BaseController
     {
         IProjectService service=new ProjectService();
+        IUserService userService = new UserService();
+        
         //
         // GET: /Project/
         public ActionResult Index()
         {
-            var projectList=service.LoadEntities(p => true);
+           var projectList = service.LoadEntities(p => true);
             return View(projectList);
+             //return View();
         }
+        [HttpGet]
+        public JsonResult GetProject()
+        {
+          var Project=  service.LoadEntities(p => true);
+          foreach (var item in Project)
+          {
+              
+          }
+          var Projectlis = Project.Select(x => new { Name = x.Name, UName = x.User.Name, StartDate =x.StartDate, Description = x.Description, Status = x.Status, Color = x.Color}).AsEnumerable();
+          return Json(Projectlis, JsonRequestBehavior.AllowGet);
+        }
+
+  
 	}
 }
